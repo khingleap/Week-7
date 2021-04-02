044f465eba07e61e001013722e5900a248d4a17a
khingleap <75134551+khingleap@users.noreply.github.com>
2021-04-02 22:02:30 +0700

    Delete Khingleap_Lab4/Khingleap_Lab4 directory

--- Khingleap_Lab4/Khingleap_Lab4/Task2/Interface/sale.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    interface sale{
-        public function getSaleAmount();
-        public function getItem();
-        public function getPrice();
-        public function getQuantity();
-    }
-?>
